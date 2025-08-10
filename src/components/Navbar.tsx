9ed83a161366463994c404f1c252140134554098
Bikash <bikashjha490@gmail.com>
2025-08-10 23:21:54 +0545

    initial

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+function Navbar() {
+  return (
+    <div className='flex justify-between item-center min-h-[50px] border-2 pt-6'>
+       <div className='flex justify-between item-center w-[30%]'>
+          <p className='text-regal-pink font-bold'>Multiswap</p>
+       </div>
+  
+    </div>
+  )
+}
+
+export default Navbar;
